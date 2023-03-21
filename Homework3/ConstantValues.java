7d955129208212fcb16e8f5fc3015f122a10131d
L0KK11 <myllymakiossi@gmail.com>
2023-03-21 13:35:20 +0200

    .

--- Homework3/ConstantValues.java
+++ Homework3/ConstantValues.java
@@ -1,4 +1,4 @@
-//package dev.m3s.programming2.homework2;
+//package dev.m3s.programming2.homework3;
 
 import java.time.Year;
 
@@ -10,25 +10,38 @@ public class ConstantValues {
     public static final String NO_BIRTHDATE = "Not available";
     public static final String INVALID_BIRTHDAY = "Invalid birthday!";
     public static final String INCORRECT_CHECKMARK = "Incorrect check mark!";
-    public static final int MIN_ID = 1;
-    public static final int MAX_ID = 100;
+    public static final String NOT_AVAILABLE = "Not available";
 
+    public static final int MIN_STUDENT_ID = 1;
+    public static final int MAX_STUDENT_ID = 100;
+    public static final int MIN_EMP_ID = 2001;
+    public static final int MAX_EMP_ID = 3000;
     public static final double MIN_CREDITS = 0.0;
+  
     public static final double MAX_CREDITS = 300.0;
     public static final double BACHELOR_CREDITS = 180.0;
     public static final double MASTER_CREDITS = 120.0;
-
+    public static final double BACHELOR_OPTIONAL = 30.0;
+    public static final double MASTER_OPTIONAL = 70.0;
+    public static final double BACHELOR_MANDATORY = 150.0;
+    public static final double MASTER_MANDATORY = 50.0;
     public static final double MAX_COURSE_CREDITS = 55.0;
+
     public static final int MIN_PERIOD = 1;
     public static final int MAX_PERIOD = 5;
+
     public static final int BACHELOR_TYPE = 0;
     public static final int MASTER_TYPE = 1;
     public static final int OPTIONAL = 0;
     public static final int MANDATORY = 1;
     public static final int ALL = 2;
-
     public static final int MIN_GRADE = 0;
     public static final int MAX_GRADE = 5;
     public static final char GRADE_FAILED = 'F';
     public static final char GRADE_ACCEPTED = 'A';
+
+    public static final String BACHELOR_STR = "Bachelor Degree";
+    public static final String MASTER_STR = "Masters Degree";
+    public static final String DOCTORAL_STR = "Doctoral Degree";
+
 }
