c50c448a5238e9d7c82123d97cec727206b78e5d
L0KK11 <myllymakiossi@gmail.com>
2023-03-29 11:04:42 +0300

    .

---
+++ Homework3/Employee.java
@@ -0,0 +1,45 @@
+public abstract class Employee extends Person implements Payment {
+    
+    //Attributes
+    private String empId;
+    private int startYear = ConstantValues.CURRENT_YEAR;
+    private Payment payment;
+
+    //Constructors
+
+    public Employee(String lname, String fname) {
+        super(lname, fname);
+        int id = super.getRandomId(2001, 3000);
+        empId = "OY_" + id; 
+    }
+
+    //Methods
+    public String getIdString() {
+        return empId;
+    }
+
+    public int getStartYear() {
+        return startYear;
+    }
+
+    public void setStartYear(final int startYear) {
+        if (startYear > 2000 && startYear <= ConstantValues.CURRENT_YEAR) {
+            this.startYear = startYear;
+        }
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        if (payment != null) {
+            this.payment = payment;
+        }
+    }
+
+    public double calculatePayment() {
+        
+    }
+
+}
---
+++ Homework3/Payment.java
@@ -0,0 +1,5 @@
+public interface Payment {
+    
+    public double calculatePayment();
+
+}
