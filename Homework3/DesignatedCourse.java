cc213f00e84510e39fc6bae834314cf0d4a9d8fe
L0KK11 <myllymakiossi@gmail.com>
2023-04-05 13:56:37 +0300

    .

--- Homework3/DesignatedCourse.java
+++ Homework3/DesignatedCourse.java
@@ -21,5 +21,36 @@ public class DesignatedCourse {
         return course;
     }
 
-    
+    public void setCourse(Course course) {
+        if (course != null) {
+            this.course = course;
+        }
+    }
+
+    public boolean isResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(boolean responsible) {
+        this.responsible = responsible;
+    }
+
+    public boolean getResponsible() {
+        return responsible;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        if (year >= 2000 && year <= ConstantValues.CURRENT_YEAR + 1) {
+            this.year = year;
+        }
+    }
+
+    public String toString() {
+        return
+            course.toString() + ", year=" + year + "]"; 
+    }
 }
